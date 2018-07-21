7e95c50a552148e5a17b7eb741f9558927d5c1cd
dani quinn sexton <spells@witch.zone>
2018-07-22 00:31:14 +0100

    Add clear buttons to optional shelves

--- src/app/components/Wardrobe/Shelf.jsx
+++ src/app/components/Wardrobe/Shelf.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import { replace, toUpper } from 'ramda'
+
+const capitalise = replace(/^./, toUpper)
 
 const getShelfLabel = (shelf, item) => (
   shelf === 'glasses'
     ? `Try on the ${item} ${shelf}!`
-    : `Wear ${item}'s ${shelf}!`
+    : `Try on ${capitalise(item)}'s ${shelf}!`
 )
 
 const Shelf = ({
@@ -22,20 +25,34 @@ const Shelf = ({
     )}
   >
     {items.map((item) => (
-      <button
-        key={item.name}
-        type="button"
-        title={getShelfLabel(name, item.name)}
-        className={classnames(
-          'c-wardrobe__button',
-          'c-look-icon',
-          item.name,
-          {
-            'is-selected': selected === item.name,
-          },
-        )}
-        onClick={() => onClickItem(item.name)}
-      />
+      item.name === 'nothing' ? (
+        <button
+          key={item.name}
+          type="button"
+          className={classnames(
+            'c-wardrobe__clear-button',
+            'c-plain-button',
+            'c-clear-button',
+          )}
+          title={`Remove your ${name}!`}
+          onClick={() => onClickItem(item.name)}
+        />
+      ) : (
+        <button
+          key={item.name}
+          type="button"
+          title={getShelfLabel(name, item.name)}
+          className={classnames(
+            'c-wardrobe__button',
+            'c-look-icon',
+            item.name,
+            {
+              'is-selected': selected === item.name,
+            },
+          )}
+          onClick={() => onClickItem(item.name)}
+        />
+      )
     ))}
   </div>
 )
